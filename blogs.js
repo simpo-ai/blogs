eba3c5c03dd695c2b8bd824a70b341fb1b990e2f
Durga250720 <76244621+Durga250720@users.noreply.github.com>
2024-02-14 17:47:51 +0530

    All file moved into git humb

---
+++ blogs.js
@@ -0,0 +1,18 @@
+
+class allBlogs {
+    constructor() {
+        setTimeout(() => {
+            let a = document.getElementById('all-blogs')
+            let accessKey = document.getElementById('all-blogs').accessKey;
+            window.localStorage.setItem('id',accessKey);
+            window.localStorage.removeItem('blogDetail')
+            console.log(a)
+            if (!a) return;
+            a.innerHTML += `
+            <iframe id="myHtml" src="index.html" style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
+            `
+        }, 300);
+    }
+}
+
+listBlogs = new allBlogs();
