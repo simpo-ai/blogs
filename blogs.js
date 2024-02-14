1c662e6f924be86d82650f4a88334dc1ee7af0f0
Durga250720 <76244621+Durga250720@users.noreply.github.com>
2024-02-15 02:12:03 +0530

    Update blogs.js

--- blogs.js
+++ blogs.js
@@ -5,7 +5,6 @@ class allBlogs {
             let a = document.getElementById('all-blogs')
             var accessKey = document.getElementById('all-blogs').accessKey;
             localStorage.removeItem('blogDetail')
-            console.log(a)
             if (!a) return;
             a.innerHTML += `
             <iframe id="myHtml" src=https://durga250720.github.io/blogs/index.html?id=${accessKey} style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
