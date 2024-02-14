f11e219146bcc64fc637b22d4e5ad79a7ccf289b
Durga250720 <76244621+Durga250720@users.noreply.github.com>
2024-02-15 00:17:58 +0530

    checks

--- blogs.js
+++ blogs.js
@@ -4,13 +4,15 @@ class allBlogs {
         setTimeout(() => {
             let a = document.getElementById('all-blogs')
             let accessKey = document.getElementById('all-blogs').accessKey;
-            localStorage.setItem('id',accessKey);
             localStorage.removeItem('blogDetail')
             console.log(a)
             if (!a) return;
             a.innerHTML += `
             <iframe id="myHtml" src="https://durga250720.github.io/blogs/index.html" style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
             `
+            setTimeout(()=>{
+                window.localStorage.setItem('id',accessKey);
+            },300)
         }, 300);
     }
 }
