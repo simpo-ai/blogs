6322031ba2409558c0d028436e7d35d60b610994
Durga250720 <76244621+Durga250720@users.noreply.github.com>
2024-02-15 03:14:12 +0530

    updated main file for home page and list page

--- blogs.js
+++ blogs.js
@@ -2,13 +2,28 @@
 class allBlogs {
     constructor() {
         setTimeout(() => {
-            let a = document.getElementById('all-blogs')
-            var accessKey = document.getElementById('all-blogs').accessKey;
-            localStorage.removeItem('blogDetail')
-            if (!a) return;
-            a.innerHTML += `
-            <iframe id="myHtml" src=https://durga250720.github.io/blogs/index.html?id=${accessKey} style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
-            `
+           if(document.getElementById('all-blogs') != null){
+                let a = document.getElementById('all-blogs')
+                accessKey = document.getElementById('all-blogs').accessKey;
+                localStorage.setItem('id',accessKey);
+                window.localStorage.removeItem('blogDetail')
+                console.log(a)
+                if (!a) return;
+                a.innerHTML += `
+                <iframe id="myHtml" src=index.html?id=${accessKey} style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
+                `
+            }
+            else{
+                let a = document.getElementById('homePageDispaly')
+                accessKey = document.getElementById('homePageDispaly').accessKey;
+                localStorage.setItem('id',accessKey);
+                window.localStorage.removeItem('blogDetail')
+                console.log(a)
+                if (!a) return;
+                a.innerHTML += `
+                <iframe id="myHtml" src=home-page.html?id=${accessKey} style="width:100%;height:calc(100vh - 20px);border:none"></iframe>
+                `
+            }
         }, 300);
     }
 }
